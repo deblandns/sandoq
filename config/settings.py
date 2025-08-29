3fa797d5bd79fed45dcb46800b9876fc6d438f80
deblandns <hoseinnysyan1385@gmail.com>
2025-08-29 15:31:49 +0330

    remove the find_dotenv that was for test

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
